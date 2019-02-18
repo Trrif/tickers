e083c07bacc55ece076dacd4a5ef30142f35b07f
trriforce <ssh.source@gmail.com>
2019-02-18 06:29:02 +0400

    Updates

---
+++ precache-manifest.8291c446d046a30d72f5627eb1aa8c8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ed6ade8ef3ca884b104",
+    "url": "/tickers/static/css/main.3de34311.chunk.css"
+  },
+  {
+    "revision": "5ed6ade8ef3ca884b104",
+    "url": "/tickers/static/js/main.5ed6ade8.chunk.js"
+  },
+  {
+    "revision": "f863f92d6e84621d407c",
+    "url": "/tickers/static/js/runtime~main.f863f92d.js"
+  },
+  {
+    "revision": "b530f53cc9ced95923bb",
+    "url": "/tickers/static/js/2.b530f53c.chunk.js"
+  },
+  {
+    "revision": "b92087b76f807c59298ff550f3e15dfa",
+    "url": "/tickers/index.html"
+  }
+];

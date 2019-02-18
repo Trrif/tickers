f7f19925bfc4819dc0e6b98389117f10260829c5
trriforce <ssh.source@gmail.com>
2019-02-18 05:55:03 +0400

    Init Main files and apply prettier

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import {
+  refreshAllTickers,
+  refreshTicker,
+  addCurrency,
+  removeCurrency,
+  setRefreshInterval
+} from "./store/actions";
+
+import Layout from "./components/Layout";
+import {saveStore} from "./utils/storeSaving";
 
 class App extends Component {
+  componentWillUpdate(nextProps) {
+      saveStore(nextProps.tickers)
+  }
+
+componentDidMount() {
+    this.props.setRefreshInterval(2500);
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <Layout {...this.props} />;
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    tickers: state
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    refreshAllTickers,
+    refreshTicker,
+    addCurrency,
+    removeCurrency,
+    setRefreshInterval
+  }
+)(App);
